6d0a674be7370027622d0c50e1149bd23ac18731
Mark Peters <mapeters101@earthlink.net>
2025-01-18 13:47:17 -0500

    Work on lesson012.

---
+++ lesson012.py
@@ -0,0 +1,11 @@
+# number = input("Enter a number: ")
+# print(f"The number of digits in {number} is {len(number)}")
+# print()
+#
+# count = 0
+# for char in number:
+#     count += 1
+#
+# print(f"The number of digits in {number} is {count}")
+
+print("Number of digits", len(input("Enter a number")))
